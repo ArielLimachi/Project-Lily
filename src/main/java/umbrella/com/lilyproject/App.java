8ab396b77366aaefb424993bdff77a4eb9b317c0
ArielLimachi <ariecoenki@gmail.com>
2023-04-21 13:52:07 -0400

    initial commit

---
+++ src/main/java/umbrella/com/lilyproject/App.java
@@ -0,0 +1,13 @@
+package umbrella.com.lilyproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
