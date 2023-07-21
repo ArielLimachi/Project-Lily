883c71045c7343769f019adbd34bc59c39942904
ArielLimachi <89421091+ArielLimachi@users.noreply.github.com>
2023-07-21 07:52:35 -0400

    feature/FTR-6 Added basic usb-arduino communication using JSerialComm Library (#6)

--- src/main/java/umbrella/com/lilyproject/App.java
+++ src/main/java/umbrella/com/lilyproject/App.java
@@ -1,6 +1,7 @@
 package umbrella.com.lilyproject;
 
 import umbrella.com.lilyproject.ui.TrashFrame;
+import umbrella.com.lilyproject.usb.ArduinoSerialUtility;
 
 /**
  * Hello world!
@@ -8,8 +9,45 @@ import umbrella.com.lilyproject.ui.TrashFrame;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
-		TrashFrame frame = new TrashFrame();
+
+		// System.out.println("Hello World!");
+
+		// TrashFrame frame = new TrashFrame();
+
+		ArduinoSerialUtility serialUtility = new ArduinoSerialUtility();
+
+		// Replace "COM3" with the correct port name for your Arduino
+		String portName = "COM4";
+		int baudRate = 9600;
+
+		// Open the port
+		if (serialUtility.openPort(portName, baudRate)) {
+			System.out.println("Port opened successfully.");
+
+			// Read and print data from Arduino
+			for (int i = 0; i < 5; i++) {
+				String receivedData = serialUtility.readData();
+				if (receivedData != null) {
+					System.out.println("Received data: " + receivedData);
+				} else {
+					System.err.println("Failed to read data.");
+				}
+
+				try {
+					Thread.sleep(2000); // Wait for 2 seconds before reading the next data packet
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// Close the port
+			if (serialUtility.closePort()) {
+				System.out.println("Port closed.");
+			} else {
+				System.err.println("Failed to close the port.");
+			}
+		} else {
+			System.err.println("Failed to open the port.");
+		}
 	}
 }
